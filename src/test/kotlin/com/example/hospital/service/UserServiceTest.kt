eea553b35ef5e29ecfd7bba31543443b1221ab77
kpvelez <83775509+kpvelez@users.noreply.github.com>
2022-01-22 20:06:15 -0500

    S14 - Pruebas unitarias para las clases service

--- src/test/kotlin/com/example/hospital/service/UserServiceTest.kt
+++ src/test/kotlin/com/example/hospital/service/UserServiceTest.kt
@@ -1,16 +1,30 @@
 package com.example.hospital.service
 
 
+
+import com.example.hospital.model.User
+import com.example.hospital.repository.UserRepository
+import com.google.gson.Gson
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.io.File
 
 @SpringBootTest
 class UserServiceTest {
 
     @Autowired
     lateinit var userService: UserService
+
+    @InjectMocks
+    lateinit var userServic: UserService
+
+    @Mock
+    lateinit var userRepository: UserRepository
 
     @Test
     fun calcMultiplicationIfIsPair(){
@@ -52,5 +66,79 @@ class UserServiceTest {
         Assertions.assertEquals(true, response)
     }
 
+
+
+    val returnObject: User = User().apply {
+        id= 1
+        username="Paul"
+        password= "2001"
+        cedula= "0106362338"
+    }
+    val newObject: User = User().apply {
+        id= 1
+        username="Paul"
+        password= "2001"
+        cedula= "0106362338"
+    }
+
+    @Test
+    fun saveIsCorrect(){
+        Mockito.`when`(userRepository.save(Mockito.any(User::class.java))).thenReturn(returnObject)
+        val response = userServic.save(newObject)
+        Assertions.assertEquals(response.id, newObject.id)
+        Assertions.assertEquals(response.username, newObject.username)
+        Assertions.assertEquals(response.password, newObject.password)
+        Assertions.assertEquals(response.cedula, newObject.cedula)
+
+    }
+
+    val jsonString = File("./src/test/resources/user/crearUser.json").readText(Charsets.UTF_8)
+    val userMock = Gson().fromJson(jsonString, User::class.java)
+
+    @Test
+    fun saveUser(){
+        //PAra actualizar
+        //Mockito.`when`(dietRepository.findById(dietMock.id)).thenReturn(dietMock)
+        Mockito.`when`(userRepository.save(Mockito.any(User::class.java))).thenReturn(userMock)
+        val response = userServic.save(userMock)
+        Assertions.assertEquals(response.id, userMock.id)
+        Assertions.assertEquals(response.username, userMock.username)
+        Assertions.assertEquals(response.password, userMock.password)
+        Assertions.assertEquals(response.cedula, userMock.cedula)
+
+    }
+
+    @Test
+    fun saveUserFailedUserName(){
+        Assertions.assertThrows(Exception::class.java) {
+            userMock.apply { username= " "}
+
+            Mockito.`when`(userRepository.save(Mockito.any(User::class.java))).thenReturn(userMock)
+            userServic.save(userMock)
+        }
+
+    }
+
+    @Test
+    fun saveUserFailedPassword(){
+        Assertions.assertThrows(Exception::class.java) {
+            userMock.apply { password= " "}
+
+            Mockito.`when`(userRepository.save(Mockito.any(User::class.java))).thenReturn(userMock)
+            userServic.save(userMock)
+        }
+
+    }
+
+    @Test
+    fun saveUserFailedCedula(){
+        Assertions.assertThrows(Exception::class.java) {
+            userMock.apply { cedula= " "}
+
+            Mockito.`when`(userRepository.save(Mockito.any(User::class.java))).thenReturn(userMock)
+            userServic.save(userMock)
+        }
+
+    }
 
 }
